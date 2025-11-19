9330e00ff46299cef4a00c08a93faf30ff6ddfd8
NhatMinhPhan <nhatminh120767@gmail.com>
2025-11-19 12:59:25 -0500

    Built a basic frontend layout

--- Frontend/The-Scholar-Gaide/src/App.jsx
+++ Frontend/The-Scholar-Gaide/src/App.jsx
@@ -2,10 +2,13 @@ import { useState } from "react";
 //import reactLogo from "./assets/react.svg";
 //import viteLogo from "/vite.svg";
 import scholarLogo from "./assets/temp_logo.jpg";
+import minhPortrait from "./assets/Portraits/minh.jpg";
+import divitPortrait from "./assets/Portraits/divit.jpg";
+import EssayAnalysis from "./EssayAnalysis";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [analysisAvailable, setAnalysisAvailable] = useState(true);
 
   return (
     <>
@@ -23,11 +26,45 @@ function App() {
         </i>
         de
       </h1>
-      <div className="textbox essay">
-        <p>
-          Edit <code>src/App.jsx</code> and create a text box below for essays.
-        </p>
+      <div className="essay">
+        <p>Paste your essay here.</p>
+        <form>
+          <textarea
+            name="essay"
+            id="essay_textarea"
+            rows={20}
+            cols={60}
+            style={{ fontSize: "18px" }}
+            placeholder="Paste your essay in here."
+          ></textarea>
+          <br></br>
+          <input
+            type="submit"
+            className="submitEssay"
+            value={"Start analysis"}
+          ></input>
+        </form>
       </div>
+      {!analysisAvailable ? null : <EssayAnalysis />}
+      <hr></hr>
+      <div className="devs">
+        <h1>Meet the Devs!</h1>
+        <div className="devsContainer">
+          <div>
+            <img src={minhPortrait} alt="Nhat Minh Phan's Portrait"></img>
+            <h3>Nhat Minh Phan</h3>
+            <p>Programmer, Designer</p>
+          </div>
+          <div>
+            <img src={divitPortrait} alt="Nhat Minh Phan's Portrait"></img>
+            <h3>Divit Asrani</h3>
+            <p>Programmer</p>
+          </div>
+        </div>
+      </div>
+      <hr></hr>
+      <p>© The Scholar Gaide, 2025</p>
+      <p>Devpost</p>
     </>
   );
 }
---
+++ Frontend/The-Scholar-Gaide/src/EssayAnalysis.jsx
@@ -0,0 +1,10 @@
+import "./App.css";
+
+export default function EssayAnalysis() {
+  return (
+    <div>
+      <p>Here's our analysis:</p>
+      <div class="essayAnalysis">Lorem ipsum dolor sit amet</div>
+    </div>
+  );
+}
